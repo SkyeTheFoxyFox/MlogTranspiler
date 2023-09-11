93e08de5190daf322ca422a48eaaa6a5e611619c
SkyeTheFoxyFox <90353092+SkyeTheFoxyFox@users.noreply.github.com>
2023-09-10 19:53:24 -0700

    fixed arr, and redid macros
    arr now works properly
    
    macros are processed faster and the time cost of having unused macros is now negligible

--- transpiler.py
+++ transpiler.py
@@ -7,6 +7,9 @@ def ERROR(message):
 	input_code.close()
 	sys.exit()
 
+def WARNING(message):
+	print("WARNING:", message)
+
 def parse_file(input):
 	text = input.read()
 
@@ -21,7 +24,6 @@ def parse_file(input):
 		if(text[i].startswith('#')):
 			#print("clearing comment line " + str(i))
 			del(text[i])
-
 	return(text)
 
 def parse_line(input):
@@ -61,87 +63,17 @@ def instruction_arr(line, output):
 				output.append("set _" + line[2] + "_output_ _" + line[2] + "_var" + str(i))
 				output.append("set @counter _" + line[2] + "_return_pointer_")
 		case "write":
-			output.append("set _" + line[2] + "_input_ @unit ")
-			output.append("op mul _" + line[2] + "_jump_pointer_ i 3")
+			output.append("set _" + line[2] + "_input_ " + line[3])
+			output.append("op mul _" + line[2] + "_jump_pointer_ " + line[4] + " 3")
 			output.append("op add _" + line[2] + "_return_pointer_ @counter 1")
 			output.append("jump " + line[2] + "_write_ always")
 		case "read":
-			output.append("op mul _" + line[2] + "_jump_pointer_ i 3")
+			output.append("op mul _" + line[2] + "_jump_pointer_ " + line[4] + " 3")
 			output.append("op add _" + line[2] + "_return_pointer_ @counter 1")
 			output.append("jump " + line[2] + "_read_ always")
-			output.append("set unit _" + line[2] + "_output_")
+			output.append("set " + line[3] + " _" + line[2] + "_output_")
 		case _:
 			ERROR("unknown instruction \"arr " + line[1] + "\"")
-
-def macro_define_search(input):
-	output = []
-	can_macro = True
-	is_macro = False
-	current_macro_data = []
-	current_macro = ""
-	for line in input:
-		parsed_line = parse_line(line)
-		if(can_macro == True):
-			if(is_macro == True):
-				if(parsed_line[0] == "mac" and parsed_line[1] == "define"):
-					ERROR("macro \"" + current_macro + "\" not closed")
-				elif(parsed_line[0] == "mac" and parsed_line[1] == "end"):
-					is_macro = False
-					can_macro = False
-				else:
-					output_line = ""
-					if(parsed_line[0] == "mac" and parsed_line[1] == current_macro):
-						ERROR("Macro \"" + current_macro + "\" tried to call itself")
-					for word in parsed_line:
-						if word in arguments:
-							output_line = output_line + "_macvar_" + str(arguments.index(word)) + " "
-						else:
-							output_line = output_line + word + " "
-
-					current_macro_data.append(output_line)
-			elif(not (parsed_line[0] == "mac" and parsed_line[1] == "define")):
-				output.append(line)
-			if(parsed_line[0] == "mac" and parsed_line[1] == "define"):
-				current_macro = parsed_line[2]
-				arguments = parsed_line[3:]
-				is_macro = True
-		else:
-			output.append(line)
-	return(output, current_macro, current_macro_data)
-
-def macro_call_search(input, macro_name, macro_data):
-	output = []
-	macro_index = 0
-	for line in input:
-		parsed_line = parse_line(line)
-		if(parsed_line[0] == "mac" and parsed_line[1] == macro_name):
-			print("found call for macro " + macro_name)
-			arguments = parsed_line[2:]
-			for macro_line in macro_data:
-				parsed_macro_line = parse_line(macro_line)
-				output_line = ""
-				for index in range(len(parsed_macro_line)):
-					word = parsed_macro_line[index]
-					if "_macvar_" in word:
-						arg_index = int(word[8:])
-						if(len(arguments) <= arg_index):
-							ERROR("Macro \"" + macro_name + "\" needs " + str(len(arguments)) + " arguments")
-						output_line = output_line + arguments[arg_index] + " "
-					elif(word.endswith(":")):
-						output_line = output_line + "_" + macro_name + "_" + str(macro_index) + "_" + word
-					elif(parsed_macro_line[0] == "jump" and index == 1):
-						output_line = output_line + "_" + macro_name + "_" + str(macro_index) + "_" + word + " "
-					else:
-						if(is_variable(parsed_macro_line[0], word, index) and ENABLE_SCOPE):
-							output_line = output_line + "_" + macro_name + "_" + str(macro_index) + "_" + word + " "
-						else:
-							output_line = output_line + word + " "
-				output.append(output_line)
-			macro_index = macro_index + 1
-		else:
-			output.append(line)
-		
-	return(output)
 
 def is_variable(instruction, value, index):
 	return(not is_instruction_value(instruction, index) and not is_number(value) and not is_string(value) and not is_enum(value))
@@ -185,12 +117,76 @@ def is_string(value):
 def is_enum(value):
 	return(value[0] == '@')
 
+def macro_split(input, macros, macro_indices, output):
+	is_macro = False
+	current_macro = ""
+	for line in input:
+		parsed_line = parse_line(line)
 
-input_code = open(sys.argv[1], "r")
+		if(parsed_line[0] == "mac" and parsed_line[1] == "end"):
+			is_macro = False
+
+		elif(is_macro):
+			output_line = ""
+			for word in parsed_line:
+				if word in arguments:
+					output_line = output_line + "_macvar_" + str(arguments.index(word)) + " "
+				else:
+					output_line = output_line + word + " "
+			macros[current_macro].append(output_line)
+
+		elif(parsed_line[0] == "mac" and parsed_line[1] == "define"):
+			is_macro = True
+			current_macro = parsed_line[2]
+			macros[current_macro] = []
+			macro_indices[current_macro] = 0
+			arguments = parsed_line[3:]
+
+		else:
+			output.append(line)
+
+def macro_insert(macro, arguments, macro_indices, macro_list, chain_list, output):
+	chain_list.append(macro)
+	if(macro not in macro_list):
+		ERROR("macro \"" + macro + "\" undefined")
+	for line in macro_list[macro]:
+		parsed_line = parse_line(line)
+		output_line = ""
+		for index in range(len(parsed_line)):
+			word = parsed_line[index]
+			if "_macvar_" in word:
+				arg_index = int(word[8:])
+				if(len(arguments) <= arg_index):
+					ERROR("Macro \"" + macro + "\" needs more arguments")
+				output_line = output_line + arguments[arg_index] + " "
+			elif(word.endswith(":")):
+				output_line = output_line + "_" + macro + "_" + str(macro_indices[macro]) + "_" + word
+			elif(parsed_line[0] == "jump" and index == 1):
+				output_line = output_line + "_" + macro + "_" + str(macro_indices[macro]) + "_" + word + " "
+			else:
+				if(is_variable(parsed_line[0], word, index) and ENABLE_SCOPE):
+					output_line = output_line + "_" + macro + "_" + str(macro_indices[macro]) + "_" + word + " "
+				else:
+					output_line = output_line + word + " "
+		parsed_line = parse_line(output_line.strip("\t "))
+		if(parsed_line[0] == "mac"):
+			if(parsed_line[1] not in chain_list):
+				macro_insert(parsed_line[1], parsed_line[2:], macro_indices, macro_list, chain_list, output)
+			else:
+				WARNING("macro \"" + parsed_line[1] + "\" tried to call itself... skipping")
+		else:
+			output.append(output_line)
+
+	macro_indices[macro] = macro_indices[macro] + 1
+	chain_list.pop()
+
+
+
+input_file = open(sys.argv[1], "r")
 
 output_code = []
 
-for line in parse_file(input_code):
+for line in parse_file(input_file):
 	#print(line)
 	parsed_line = parse_line(line)
 	match parsed_line[0]:
@@ -199,28 +195,45 @@ for line in parse_file(input_code):
 		case _:
 			output_code.append(line)
 
-#print("Parsed normal stuff")
+input_code = output_code
+output_code = []
+macro_list = {}
+macro_indices = {}
+chain_list = []
 
-while(True):
-	output_code, macro_name, macro_data = macro_define_search(output_code)
-	if(macro_name == ""):
-		break
-	print("working on macro " + macro_name)
-	output_code = macro_call_search(output_code, macro_name, macro_data)
-for line in output_code:
+macro_split(input_code, macro_list, macro_indices, output_code)
+
+input_code = output_code
+output_code = []
+
+for line in input_code:
 	parsed_line = parse_line(line)
 	if(parsed_line[0] == "mac"):
-		ERROR("Unknown macro \"" + parsed_line[1] + '"')
+		macro_insert(parsed_line[1], parsed_line[2:], macro_indices, macro_list, chain_list, output_code)
+	else:
+		output_code.append(line)
+
+#while(True):
+#	output_code, macro_name, macro_data = macro_define_search(output_code)
+#	if(macro_name == ""):
+#		break
+#	print("working on macro " + macro_name)
+#	output_code = macro_call_search(output_code, macro_name, macro_data)
+#for line in output_code:
+#	parsed_line = parse_line(line)
+#	if(parsed_line[0] == "mac"):
+#		ERROR("Unknown macro \"" + parsed_line[1] + '"')
+
 file_output = ""
 for i in output_code:
-	file_output = file_output + i + "\n"
+	file_output = file_output + i.strip("\t ") + "\n"
 
 
 
-write_code = open(sys.argv[2], "w")
-write_code.write(file_output)
+output_file = open(sys.argv[2], "w")
+output_file.write(file_output)
 
 print("File transpiled successfully")
 
-input_code.close()
-write_code.close()
+input_file.close()
+output_file.close()
